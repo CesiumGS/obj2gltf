3169452ca077dfe869d4735596f9f2146732550f
Sean Lilley <lilleyse@gmail.com>
2016-07-20 14:23:27 -0400

    Instead of -e command line flag, use -s or -t

--- bin/obj2gltf.js
+++ bin/obj2gltf.js
@@ -11,7 +11,8 @@ if (process.argv.length < 3 || defined(argv.h) || defined(argv.help)) {
     console.log('  -i, --input             Path to obj file');
     console.log('  -o, --output            Directory or filename for the exported glTF file');
     console.log('  -b, --binary            Output binary glTF');
-    console.log('  -e, --embed             Embed glTF resources into a single file');
+    console.log('  -s --separate           Writes out separate geometry/animation data files, shader files, and textures instead of embedding them in the glTF file.');
+    console.log('  -t --separateImage      Write out separate textures only.');
     console.log('  -h, --help              Display this help');
     console.log('      --ao                Apply ambient occlusion to the converted model');
     process.exit(0);
@@ -20,7 +21,8 @@ if (process.argv.length < 3 || defined(argv.h) || defined(argv.help)) {
 var objFile = defaultValue(argv._[0], defaultValue(argv.i, argv.input));
 var outputPath = defaultValue(argv._[1], defaultValue(argv.o, argv.output));
 var binary = defaultValue(defaultValue(argv.b, argv.binary), false);
-var embed = defaultValue(defaultValue(argv.e, argv.embed), false);
+var separate = defaultValue(defaultValue(argv.s, argv.separate), false);
+var separateImage = defaultValue(defaultValue(argv.t, argv.separateImage), false);
 var quantize = defaultValue(defaultValue(argv.q, argv.quantize), false); // Undocumented option
 var ao = defaultValue(argv.ao, false);
 
@@ -32,7 +34,8 @@ console.time('Total');
 
 var options = {
     binary : binary,
-    embed : embed,
+    embed : !separate,
+    embedImage : !separateImage,
     quantize : quantize,
     ao : ao
 };
