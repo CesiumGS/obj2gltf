dc1a1976db21fab43c702ab200737951ea2fee53
Robert Taglang <minitags@comcast.net>
2016-07-22 16:17:27 -0400

    Last few tweaks

--- specs/lib/convertSpec.js
+++ specs/lib/convertSpec.js
@@ -10,7 +10,7 @@ var gltfFile = './specs/data/BoxTextured/BoxTextured.gltf';
 
 describe('convert', function() {
     it('converts an obj to gltf', function(done) {
-        var spy = spyOn(gltfPipeline, 'processJSONToDisk').and.callFake(function() {
+        var spy = spyOn(gltfPipeline, 'processJSONToDisk').and.callFake(function(gltf, outputPath, options, callback) {
             return;
         });
         expect(convert(objFile, gltfFile, {})
